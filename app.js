8217719bb5cccc1d1e0c5977b6f395e8272810ba
Ramon Mayor Martins <rmayormartins@gmail.com>
2025-10-10 21:20:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -370,4 +370,6 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
